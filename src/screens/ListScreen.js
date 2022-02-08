42790bee1dfa57344eb8c1ca92ecaa8a31779380
Paul Ogwulumba <pauleugeneogwulumba@gmail.com>
2022-02-08 21:15:45 +0100

    update

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,27 +3,26 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
     const names = [
-        { name: 'Anderson' },
-        { name: 'George' },
-        { name: 'Ugonna' },
-        { name: 'Richmond' },
-        { name: 'Samuel' },
-        { name: 'Digilus' },
+        { name: 'Anderson', age: 26 },
+        { name: 'George', age: 24 },
+        { name: 'Ugonna', age: 26 },
+        { name: 'Richmond', age: 32 },
+        { name: 'Samuel', age: 22 },
+        { name: 'Digilus', age: 21 },
     ];
 
     const renderName = ({ item }) => {
-        return <Text style={styles.nameStyle}>{item.name}</Text>
+        return <Text style={styles.nameStyle}>{item.name} is {item.age} years old!</Text>
     }
 
     return (
-        <View>
-            <Text>This is my list screen.</Text>
+        <View style={styles.divStyle}>
+            <Text style={styles.headerText}>This is my list screen.</Text>
             <FlatList 
                 data={names} 
                 renderItem={renderName} 
                 keyExtractor={({name}) => { return name } }
-                horizontal
-                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
             />
         </View>
     );
@@ -33,10 +32,21 @@ const styles = StyleSheet.create({
     nameStyle: {
         backgroundColor: 'tomato',
         padding: 30,
-        width: '100%',
+        width: 250,
         color: 'black',
-        marginVertical: 50,
+        marginVertical: 20,
         marginHorizontal: 20,
+    },
+    divStyle: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    headerText: {
+        textAlign: 'center',
+        marginVertical: 25,
+        fontSize: 20,
+        fontWeight: 'bold'
     }
 });
 
